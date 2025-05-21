b6d5f7ef6f681c0aa039b56ab29e779374087733
alancraju <146972696+alancraju@users.noreply.github.com>
2025-05-21 21:20:28 +0530

    Create dbconnection.java

---
+++ 8d.JDBC project using JDBC to update the fields/dbconnection.java
@@ -0,0 +1,16 @@
+// 8d. Develop a JDBC project using JDBC to update the fields empno, empname and basicsalary into
+// the table Emp of the database Employee using HTML and JSP to get the fields and display the
+// results respectively?
+
+package db;
+
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/Employee", "root", ""  // Use your MySQL password if set
+        );
+    }
+}
